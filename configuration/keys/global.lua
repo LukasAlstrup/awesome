44a258183179b77fc41f8203bcb4c8d06566b4ff
LukasAlstrup <lukasalstrup@protonmail.com>
2021-07-27 20:31:26 +0000

    Better indentation + bugfixes

--- configuration/keys/global.lua
+++ configuration/keys/global.lua
@@ -6,36 +6,56 @@ local hotkeys_popup = require('awful.hotkeys_popup').widget
 local modkey = require('configuration.keys.mod').modKey
 local altkey = require('configuration.keys.mod').altKey
 local apps = require('configuration.apps')
+
 local globalKeys =
   awful.util.table.join(
   awful.key({modkey}, 'F1', hotkeys_popup.show_help, {description = 'Help', group = 'Awesome'}),
+  awful.key({modkey, 'Shift'}, 'l', function() _G.exit_screen_show() end, {description = 'Logout', group = 'Awesome'}),
+  awful.key({modkey}, 'r', function() awful.spawn('rofi -combi-modi window,drun -show combi -modi combi') end, {description = 'Rofi Menu', group = 'Awesome'}),
+  awful.key({modkey, 'Control'}, 'r', _G.awesome.restart, {description = 'Reload awesome', group = 'Awesome'}),
+  awful.key({modkey}, 'x', function() awful.spawn(apps.default.terminal) end, {description = 'Terminator', group = 'Programs'}),
+  awful.key({modkey}, 'd', function() awful.client.focus.byidx(1) end, {description = 'Next', group = 'Windows'}),
   awful.key({modkey}, 'w', awful.tag.viewprev, {description = 'Next', group = 'Workspaces'}),
   awful.key({modkey}, 's', awful.tag.viewnext, {description = 'Previous', group = 'Workspaces'}),
-  awful.key({modkey},'d',function()awful.client.focus.byidx(1)end,{description = 'Next', group = 'Windows'}),
-  awful.key({modkey},'a',function()awful.client.focus.byidx(-1)end,{description = 'Previous', group = 'Windows'}),
-  awful.key({modkey},'r',function()awful.spawn('rofi -combi-modi window,drun -show combi -modi combi')end,{description = 'Main menu', group = 'Awesome'}),
-  awful.key({modkey, 'Shift'},'r',function()awful.spawn('reboot')end,{description = 'Reboot', group = 'Awesome'),
-  awful.key({modkey, 'Shift'},'s',function()awful.spawn('shutdown now')end,{description = 'Shutdown', group = 'Awesome'}),
-  awful.key({modkey, 'Shift'},'l',function()_G.exit_screen_show()end,{description = 'Logout', group = 'Awesome'}),
-  awful.key({altkey},'Tab',function()awful.client.focus.byidx(1)if _G.client.focus then _G.client.focus:raise() end end,{description = 'Next', group = 'Windows'}),
-  awful.key({altkey, 'Shift'},'Tab',function()awful.client.focus.byidx(-1)if _G.client.focus then _G.client.focus:raise() end end,{description = 'Previous', group = 'Windows'}),
-  awful.key({modkey},'l',function()awful.spawn(apps.default.lock)end,{description = 'Lock', group = 'Awesome'}),
-  awful.key({modkey},'Print',function()awful.util.spawn_with_shell(apps.default.delayed_screenshot)end,{description = 'Mark, delay 10 sec and screenshot', group = 'Screenshot'}),
-  awful.key({modkey},'p',function()awful.util.spawn_with_shell(apps.default.screenshot)end,{description = 'Screenshot active monitor', group = 'Screenshot'}),
-  awful.key({altkey, 'Shift'},'p',function()awful.util.spawn_with_shell(apps.default.region_screenshot)end,{description = 'Mark and screenshot', group = 'Screenshot'}),
-  awful.key({modkey},'c',function()awful.util.spawn(apps.default.editor)end,{description = 'Gedit', group = 'Programs'}),
-  awful.key({modkey},'b',function()awful.util.spawn(apps.default.browser)end,{description = 'Brave', group = 'Programs'}),
-  awful.key({modkey},'x',function()awful.spawn(apps.default.terminal)end,{description = 'Terminator', group = 'Programs'}),
-  awful.key({modkey, 'Control'}, 'r', _G.awesome.restart, {description = 'Reload awesome', group = 'Awesome'}),
+  awful.key({modkey}, 'o', awful.client.movetoscreen, {description = 'Move', group = 'Windows'}),
+  awful.key({modkey}, 'e', function() awful.util.spawn(apps.default.files) end, {description = 'Nemo', group = 'Programs'}),
+  awful.key({altkey, 'Shift'}, 'Up', function() awful.client.incwfact(-0.05) end, {description = 'Increase height', group = 'Layout'}),
+  awful.key({modkey}, 'space', function() awful.layout.inc(1) end, {description = 'Select next', group = 'Layout'}),
+  awful.key({modkey, 'Shift'}, 'space', function() awful.layout.inc(-1) end, {description = 'Previous', group = 'Layout'}),
   awful.key({modkey, 'Control'}, 'q', _G.awesome.quit, {description = 'Quit awesome', group = 'Awesome'}),
-  awful.key({altkey, 'Shift'},'Right',function()awful.tag.incmwfact(0.05)end,{description = 'Increase width', group = 'Layout'}),
-  awful.key({altkey, 'Shift'},'Left',function()awful.tag.incmwfact(-0.05)end,{description = 'Decrease width', group = 'Layout'}),
-  awful.key({altkey, 'Shift'},'Down',function()awful.client.incwfact(0.05)end,{description = 'Decrease height', group = 'Layout'}),
-  awful.key({altkey, 'Shift'},'Up',function()awful.client.incwfact(-0.05)end,{description = 'Increase height', group = 'Layout'}),
-  awful.key({modkey},'space',function()awful.layout.inc(1)end,{description = 'Select next', group = 'Layout'}),
-  awful.key({modkey, 'Shift'},'space',function()awful.layout.inc(-1)end,{description = 'Previous', group = 'Layout'}),
-  awful.key({modkey},'o',awful.client.movetoscreen,{description = 'Move', group = 'Windows'}),
-  awful.key({modkey},'e',function()awful.util.spawn(apps.default.files)end,{description = 'Nemo', group = 'hotkeys'})
+  awful.key({altkey, 'Shift'}, 'Right', function()awful.tag.incmwfact(0.05) end, {description = 'Increase width', group = 'Layout'}),
+  awful.key({altkey, 'Shift'}, 'Left', function()awful.tag.incmwfact(-0.05) end, {description = 'Decrease width', group = 'Layout'}),
+  awful.key({altkey, 'Shift'}, 'Down', function() awful.client.incwfact(0.05) end, {description = 'Decrease height', group = 'Layout'}),
+    awful.key({modkey}, 'l', function() awful.spawn(apps.default.lock) end, {description = 'Lock', group = 'Awesome'}),
+  awful.key({modkey}, 'Print', function() awful.util.spawn_with_shell(apps.default.delayed_screenshot) end, {description = 'Mark, delay 10 sec and screenshot', group = 'Screenshot'}),
+  awful.key({modkey}, 'p', function() awful.util.spawn_with_shell(apps.default.screenshot) end, {description = 'Screenshot active monitor', group = 'Screenshot'}),
+  awful.key({altkey, 'Shift'}, 'p', function() awful.util.spawn_with_shell(apps.default.region_screenshot) end, {description = 'Mark and screenshot', group = 'Screenshot'}),
+  awful.key({modkey}, 'c', function() awful.util.spawn(apps.default.editor) end, {description = 'Gedit', group = 'Programs'}),
+  awful.key({modkey}, 'b', function() awful.util.spawn(apps.default.browser) end, {description = 'Brave', group = 'Programs'}),
+  awful.key({modkey, 'Shift'}, 'r', function() awful.spawn('reboot') end, {description = 'Reboot', group = 'Awesome'}),
+  awful.key({modkey, 'Shift'}, 's', function() awful.spawn('shutdown now') end, {description = 'Shutdown', group = 'Awesome'}),
+  awful.key(
+  {altkey, 'Shift'},
+  'Tab',
+  function()
+    awful.client.focus.byidx(-1)
+    if _G.client.focus then
+      _G.client.focus:raise()
+    end
+  end,
+  {description = 'Previous', group = 'Windows'}),
+  
+    awful.key(
+    {altkey},
+    'Tab',
+    function()
+      awful.client.focus.byidx(1)
+      if _G.client.focus then
+        _G.client.focus:raise()
+      end
+    end,
+    {description = 'Next', group = 'Windows'}
+  )
 )
 
 for i = 1, 9 do
